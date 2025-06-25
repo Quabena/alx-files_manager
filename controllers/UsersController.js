19a6e8cc1fea779c246f3818f3431f450c380c83
Evans <ivanz.corbie@gmail.com>
2025-06-25 22:50:06 +0000

    controllers/UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -27,40 +27,42 @@ class UsersController {
         .createHash("sha1")
         .update(password)
         .digest("hex");
-      const result = await dbClient.db
-        .collection("users")
-        .insertOne({ email, password: hashedPassword });
+
+      const result = await dbClient.db.collection("users").insertOne({
+        email,
+        password: hashedPassword,
+      });
 
       return res.status(201).json({ email, id: result.insertedId });
     } catch (err) {
       return res.status(500).json({ error: "Internal server error" });
     }
   }
+
+  static async getMe(req, res) {
+    const token = req.headers["x-token"];
+
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const user = await dbClient.db
+      .collection("users")
+      .findOne({ _id: dbClient.getObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    return res.status(200).json({ email: user.email, id: user._id });
+  }
 }
-
-UsersController.getMe = async function (req, res) {
-  const token = req.headers["x-token"];
-
-  if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  const key = `auth_${token}`;
-  const userId = await redisClient.get(key);
-
-  if (!userId) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  const user = await dbClient.db
-    .collection("users")
-    .findOne({ _id: dbClient.getObjectId(userId) });
-
-  if (!user) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  return res.status(200).json({ email: user.email, id: user._id });
-};
 
 module.exports = UsersController;
